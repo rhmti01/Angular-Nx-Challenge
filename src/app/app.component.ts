83a7eff959fc18e536c22500f8deed51f9791ed0
rhmti01 <rahmati.business@gmail.com>
2025-03-22 15:30:37 +0100

    add assets and basic styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: '../styles/components/app.scss',
 })
 export class AppComponent {
-  title = 'Angular-Nx-Challenge';
+  
 }
